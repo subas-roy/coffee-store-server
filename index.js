7d6ac25f0e5c56a0f4f3a302319e8a017b4ea074
subas-roy <subasroy46@gmail.com>
2024-10-12 01:06:52 +0600

    Create basic server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Coffee server is running.')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Coffee server is runniing on port: ${port}`)
+})
