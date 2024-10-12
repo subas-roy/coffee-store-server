7b5c3653d0ef201f8cbe197a10611dd16bcef978
subas-roy <subasroy46@gmail.com>
2024-10-12 06:41:47 +0600

    Server Side hide database User and Password using Dotenv config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Middlewares
@@ -12,10 +13,7 @@ app.get('/', (req, res) => {
   res.send('Coffee server is running.')
 })
 
-// LcDG1h12R7ooejzZ
-
-const uri = "mongodb+srv://coffeeMaster:LcDG1h12R7ooejzZ@cluster0.mrd4l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrd4l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,16 +23,19 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
