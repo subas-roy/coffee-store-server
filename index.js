8a52a2fe83cd34b36844311b6c4dbcce18b6ac53
subas-roy <subasroy46@gmail.com>
2024-10-18 07:34:05 +0600

    Updated vercel.json

--- index.js
+++ index.js
@@ -26,9 +26,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
     const userCollection = client.db('coffeeDB').collection('user');
 
